b69d0960e4155352bec495e00c83558c6c58f387
mrityunjay dixit <mrityunjaydixit540@gmail.com>
2026-09-01 22:34:32 +0530

    update portfolio

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-  strings: ["Frontend Developer", "Youtuber", "Web Developer"],
+  strings: ["Student", "Web Designer", "Web Developer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 100,
